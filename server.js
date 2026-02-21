ad6c50c8bdac75091913b22102c1777b43455a6e
Elena Martínez <elenams2000@hotmail.com>
2026-02-21 12:58:03 +0100

    Fix mongoose connection options

--- server.js
+++ server.js
@@ -13,10 +13,7 @@ app.use(express.json());
 
 // MongoDB connection
 mongoose
-  .connect(process.env.MONGODB_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
+  .connect(process.env.MONGODB_URI)
   .then(() => console.log('Connected to MongoDB'))
   .catch((err) => console.error('Error connecting to MongoDB:', err));
 
